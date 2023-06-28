1de8a0a2755b7c2b15a0c571c5f6c20f2d023f41
SatrioPb <satriogaming1@gmail.com>
2023-06-28 23:04:38 +0700

    update add print

--- dashboard.php
+++ dashboard.php
@@ -169,7 +169,7 @@ $totalbarang = $barang->fetch_row()[0];
                   <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                   Profile
                 </a>
-             
+
                 <div class="dropdown-divider"></div>
                 <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
                   <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
@@ -186,7 +186,8 @@ $totalbarang = $barang->fetch_row()[0];
           <!-- Page Heading -->
           <div class="d-sm-flex align-items-center justify-content-between mb-4">
             <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
-            <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
+            <a href="print.php" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50">
+              </i> Generate Report</a>
           </div>
 
           <!-- Content Row -->
